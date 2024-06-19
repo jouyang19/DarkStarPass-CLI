49425a5fb7632a4911714239c19a4a98bc0fc93f
Guang <jouyang19@proton.me>
2024-06-18 21:34:59 -0400

    update UI, add view and edit entry

--- lib/cli.py
+++ lib/cli.py
@@ -6,7 +6,6 @@ user_id = None
 def sign_up():
     new_username = input("Username: ")
     new_password = input("Password: ")
-    User.create_table()
     user = User.create(new_username, new_password)
     global user_id
     user_id = user.id
@@ -15,7 +14,6 @@ def sign_up():
 def log_in():
     username = input("Username: ")
     password = input("Password: ")
-    User.create_table()
     user = User.find_by_name(username)
     if user:
         if password == user.password:
@@ -27,8 +25,8 @@ def log_in():
         return main()
     
     
+    
 def view_vault():
-    Password.create_table()
     for item in Password.find_all_by_user_id(user_id):
         print(item)
     print("""
@@ -37,17 +35,110 @@ def view_vault():
     (3) Delete Entry
     (4) Go Back
     """)
+    
+    def select():
+        choice = input("select an option: ")
+        if choice == "1":
+            entry_id = input("Enter Account #: ")
+                
+            # if entry_id.isdigit():
+            #     view_entry(entry_id)
+            # elif type(entry_id) is not int:
+            #     print("Please try again.")
+            #     select()
+            # else:
+            #     return view_vault()
+            view_entry(entry_id)
+        elif choice == "2":
+            entry_id = input("Enter Account #: ")
+            return edit_entry(entry_id)
+        elif choice == "3":
+            pass
+        elif choice == "4": 
+            return user_dashboard()
+        else:
+            return view_vault()
+        
+    select()
+    
+    
+def view_entry(entry_id):
+    
+    entry = Password.find_by_id(entry_id)
+    
+    print(f'''
+    ===============================================
+    Account Entry # {entry.id}
+    
+    *************** Account Details ***************
+    
+    Title: {entry.title}
+    
+    Username: {entry.username}
+    Password: {entry.password}
+    
+                                         (1) Edit    
+    (2) Back                             (3) Delete
+    ===============================================
+    ''')
+    
     choice = input("select an option: ")
     if choice == "1":
-        pass
+        edit_entry(entry.id)
     elif choice == "2":
-        pass
+        return view_vault()
     elif choice == "3":
         pass
-    elif choice == "4": 
-        return user_dashboard()
+    else:
+        return view_entry(entry.id)
     
-
+def edit_entry(entry_id): 
+    
+    entry = Password.find_by_id(entry_id)
+    
+    print(f'''
+    ===============================================
+    Account Entry # {entry.id}
+    
+    *************** Account Details ***************
+    
+    Title: {entry.title}
+    
+    Username: {entry.username}
+    Password: {entry.password}
+    
+    Which field would you like to edit:
+    
+    (1) Title? or (2) Username? or (3) Password?
+                                           
+    (4) Back                       (5) Delete entry                        
+    ===============================================
+    ''')
+    choice = input("select an option: ")
+    if choice == "1":
+        new_title = input("Enter new title: ")
+        entry.title = new_title
+        entry.update()
+        print("Title updated successfully!")
+        return edit_entry(entry.id)
+    elif choice == "2":
+        new_username = input("Enter new username: ")
+        entry.username = new_username
+        entry.update()
+        print("Username updated successfully!")
+        return edit_entry(entry.id)
+    elif choice == "3":
+        new_password = input("Enter new password: ")
+        entry.password = new_password
+        entry.update()
+        print("Password updated successfully!")
+        return edit_entry(entry.id)
+    elif choice == "4":
+        return view_entry(entry.id)
+    elif choice == "5":
+        pass
+    else:
+        return edit_entry(entry.id)
 
 def user_dashboard():
     print("""
@@ -60,6 +151,7 @@ def user_dashboard():
     if choice == "1":
         view_vault()
     elif choice == "2":
+        Password.create_table()
         title = input("Account Title: ")
         username = input("Account Username: ")
         password = input("Account Password: ")
@@ -69,15 +161,10 @@ def user_dashboard():
     elif choice == "3":
         pass
     elif choice == "4":
-        global user_id
         user_id = None
         return main()
-        
-def password_vault():
-    pass
-
-def view_entry():
-    pass
+    else:
+        return user_dashboard()
 
 def main():
 
@@ -99,4 +186,7 @@ def main():
             log_in()
 
 if __name__ == "__main__":
+    Password.create_table()
+    User.create_table()
     main()
+    
